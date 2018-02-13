8ade66317c613a4a45f5854efc901d3ee41ceebb
Unknown <luismiguel92_@hotmail.com>
2018-02-13 09:29:04 -0500

    Primera clase
    clase con suma de numeros enteros

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main{
+
+      public static void main(String[] arg){
+
+       
+               System.out.println("Clase de programacion 2");
+               System.out.println("");
+               System.out.println("Ejercicio 1");
+               
+                  int var1=50;
+                  int var2=20;
+
+                   System.out.println("La suma de la variable"+(var1+var2));
+                   
+    
+      
+    }
+
+}
